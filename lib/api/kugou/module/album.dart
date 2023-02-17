a6817161fdede8f12d59fdfa54dd7ef04a5f030b
永恒瞬间 <1130402124@qq.com>
2023-02-17 17:23:00 +0800

    优化kg专辑歌曲

--- lib/api/kugou/module/album.dart
+++ lib/api/kugou/module/album.dart
@@ -32,5 +32,24 @@ Future<Answer> _albumSong(Map params, List<Cookie> cookie) {
       "pagesize": -1,
     },
     cookie: cookie,
-  );
+  ).then((value) async {
+    try {
+      var data = value.data;
+
+      var songs=[];
+
+
+
+      var datas = (await Future.wait((data["data"]["info"] as List).map((e) => _musicInfoWithLyric({"hash": e["hash"], "albumAudioId": e["album_audio_id"]}, cookie))))
+          .map((e) => e.data["data"])
+          .toList();
+
+      data["data"]["info"] = datas;
+
+      return value.copy(data: data);
+    } catch (e) {
+      print(e);
+    }
+    return value;
+  });
 }
